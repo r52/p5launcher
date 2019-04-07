b6e00fbde523b4e76a5c01aa5d520546a7b9b64d
Simon <homer12345@gmail.com>
2019-04-07 16:38:06 -0700

    Updated for Quasar 2.0

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 var websocket = null;
 
-var msg = {
-    widget: qWidgetName,
-    type: "launcher"
-};
-
 var item = '<div class="item"><div class="bg1"></div><div class="bg2"></div></div>';
 
-$.fn.random = function() {
+$.fn.random = function () {
     var ret = $();
 
     if (this.length > 0)
@@ -16,8 +11,15 @@ $.fn.random = function() {
     return ret;
 };
 
-function launch(app) {
-    msg["app"] = app;
+function launch(cmd) {
+    var msg = {
+        "method": "query",
+        "params": {
+            "target": "launcher",
+            "params": cmd
+        }
+    }
+
     websocket.send(JSON.stringify(msg));
 }
 
@@ -38,7 +40,7 @@ function create_item(name) {
 
     $a.lettering();
     $a.children().random().addClass("box");
-    $a.children().each(function() {
+    $a.children().each(function () {
         var rotate = getRandomInt(-5, 5);
         $(this).css("--rot", rotate + "deg");
     });
@@ -49,7 +51,7 @@ function create_item(name) {
 }
 
 function randomize_arrows() {
-    $(".bg2").each(function() {
+    $(".bg2").each(function () {
         var randy = getRandomInt(0, 1);
         $(this).css("--arrowy", (randy ? 30 : -15) + "px");
     });
@@ -60,7 +62,7 @@ function rotate_column(items, right) {
         var angle_inc = Math.ceil(120.0 / (items.length + 1));
         var inc = 1;
 
-        items.each(function() {
+        items.each(function () {
             var rotate = (60 - (inc * angle_inc)) * (right ? -1 : 1);
             $(this).css("--rot", rotate + "deg");
             inc++;
@@ -68,7 +70,7 @@ function rotate_column(items, right) {
     }
 }
 
-function generate_items() {
+function generate_custom_items() {
     for (var i = 0; i < left_apps.length; i++) {
         $(".left").append(create_item(left_apps[i]));
     }
@@ -80,23 +82,59 @@ function generate_items() {
     randomize_arrows();
     rotate_column($(".left .item"), false);
     rotate_column($(".right .item"), true);
+
+    $("a").click(function () {
+        launch(this.id);
+    });
 }
 
-$(function() {
-    generate_items();
+function create_launcher(data) {
+    var entries = data["data"]["launcher"];
 
+    if (entries.length > 0) {
+        entries.forEach(function (e, i) {
+            var t = create_item(e.command);
+            $((i % 2) ? ".right" : ".left").append(t);
+        });
+
+        randomize_arrows();
+        rotate_column($(".left .item"), false);
+        rotate_column($(".right .item"), true);
+
+        $("a").click(function () {
+            launch(this.id);
+        });
+    }
+}
+
+function parse_msg(msg) {
+    var data = JSON.parse(msg);
+
+    if ("data" in data && "launcher" in data["data"]) {
+        create_launcher(data);
+    }
+}
+
+$(function () {
     try {
         if (websocket && websocket.readyState == 1)
             websocket.close();
-        websocket = new WebSocket(qWsServerUrl);
-        websocket.onerror = function(evt) {
+        websocket = quasar_create_websocket();
+        websocket.onopen = function (evt) {
+            quasar_authenticate(websocket);
+            if (autoPopulate) {
+                launch("get");
+            } else {
+                generate_custom_items();
+            }
+        };
+        websocket.onmessage = function (evt) {
+            parse_msg(evt.data);
+        };
+        websocket.onerror = function (evt) {
             console.log('ERROR: ' + evt.data);
         };
     } catch (exception) {
         console.log('Exception: ' + exception);
     }
-
-    $("a").click(function() {
-        launch(this.id);
-    });
 });
--- js/options.js
+++ js/options.js
@@ -1,2 +1,7 @@
+// Set to true to auto populate launcher commands from Quasar data
+// Set to false to manually customize using the options below
+var autoPopulate = true;
+
+// Manually customize and populate menu options if autoPopulate is set to false
 var left_apps = ["chrome", "steam"];
 var right_apps = ["spotify", "explorer"];
